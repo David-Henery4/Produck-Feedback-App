4b3d36e877a0df81e8a6e3745b4afdea37ccfff2
DizzlyD <david4henery00@hotmail.com>
2023-09-25 23:36:56 +0100

    added-reply-ui

--- src/components/detail-sections/FeedbackComments.jsx
+++ src/components/detail-sections/FeedbackComments.jsx
@@ -1,24 +1,35 @@
-import {avatarJackson} from "public/assets/user-images"
+import { avatarJackson } from "public/assets/user-images";
 import { Comment } from "./comments-components";
 
 const FeedbackComments = ({ comments }) => {
   return (
-    <div className="w-full p-6 bg-white rounded-xl tab:px-8">
+    <div className="w-full p-6 bg-white rounded-xl tab:px-8 tab:pb-8">
       <h1 className="font-bold text-lg text-lightNavy">
         {comments ? comments?.length : 0} Comments
       </h1>
 
       {/* Comments */}
       <div className="w-full mt-6">
-        {comments?.map(comment => {
-          return <Comment key={comment?.id} />
+        {comments?.map((comment) => {
+          return (
+            <div
+              className="w-full relative border-t-lightGray/25 border-t first:border-none"
+              key={comment?.id}
+            >
+              {comment?.replies && <div className="w-[1px] h-1/2 bg-gray absolute top-1/3 left-0"></div>}
+              <Comment {...comment} />
+              <div className="w-full grid gap-6 lgTab:gap-4">
+                {comment?.replies &&
+                  comment?.replies?.map((reply, i) => {
+                    return <Comment key={i} {...reply} isReply={true} />;
+                  })}
+              </div>
+            </div>
+          );
         })}
-        {/* Comment */}
-        {/* <Comment /> */}
-        {/* <Comment /> */}
       </div>
     </div>
   );
 };
 
-export default FeedbackComments
+export default FeedbackComments;
