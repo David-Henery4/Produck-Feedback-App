2782a7404d3d710ba659484d40203d87b73bd2e2
DizzlyD <david4henery00@hotmail.com>
2023-10-11 13:52:07 +0100

    pre-roadmap-animation-&-functionality-setup

--- src/components/roadmap-sections/sections/StatusToggles.jsx
+++ src/components/roadmap-sections/sections/StatusToggles.jsx
@@ -1,21 +1,15 @@
-
-
 const StatusToggles = () => {
   return (
     <div className="w-full col-start-1 col-end-13 border-b border-b-lightGray/25 lgTab:hidden">
       <div className="w-full flex justify-between items-center text-sm text-lightNavy/40 font-bold max-w-[500px] mx-auto">
-        <button className="px-7 py-5">
-          Planned (2)
-        </button>
+        <button className="px-7 py-5">Planned (2)</button>
         <button className=" text-lightNavy px-7 py-5 border-b-4 border-b-purple">
-          In-Progress (3)
+          <p className="scale-110 transition-all">In-Progress (3)</p>
         </button>
-        <button className="px-7 py-5">
-          Live (1)
-        </button>
+        <button className="px-7 py-5">Live (1)</button>
       </div>
     </div>
   );
-}
+};
 
-export default StatusToggles
+export default StatusToggles;
